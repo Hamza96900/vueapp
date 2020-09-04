be360505c0ddc097c6686a2202876031d7968287
Hamza Khan <hamza.khan@ki5.co.uk>
2020-09-04 16:16:41 +0500

    Initial commits

---
+++ src/components/App-Item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <section class="Header">
+      <h1 id="Fruiticious!">Fruiticious!</h1>
+      <!-- Cart component -->
+      <!-- <shop-cart :cart="this.cart" :total="this.total" @empty-cart="emptyCart"></shop-cart> -->
+    </section>
+    <!-- Item component -->
+    <shop-item @update-cart="updateCart"></shop-item>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      cart: [],
+      total: 0,
+    };
+  },
+  methods: {
+    updateCart(e) {
+      this.cart.push(e);
+      this.total = this.shoppingCartTotal;
+    },
+    emptyCart() {
+      this.cart = [];
+      this.total = 0;
+    },
+  },
+};
+</script>
