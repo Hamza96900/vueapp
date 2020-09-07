e62e4c98e21bab94172a73936ea8ddcaf69eca07
Hamza Khan <hamza.khan@ki5.co.uk>
2020-09-07 18:11:49 +0500

    header and footer of portfolio added

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Footer">
+  <footer class="w3-container w3-teal w3-center w3-margin-top">
+  <p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+</footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ul li {
+    display: inline-flex;
+}
+</style>
