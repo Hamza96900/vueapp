8d55611d1d253bc0eb467bef9612b8bfd6d5719f
Hamza Khan <hamza.khan@ki5.co.uk>
2020-09-10 15:29:16 +0500

    router links added to nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-    <div>
-      <Footer />
-      <Header />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
   </div>
 </template>
-<script>
-import Footer from '@/components/Header.vue'
-import Header from '@/components/Footer.vue'
-export default {
-  components:{
-    Header,
-    Footer
-  }
-}
-</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -25,5 +17,16 @@ export default {
   color: #2c3e50;
 }
 
+#nav {
+  padding: 30px;
+}
 
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
